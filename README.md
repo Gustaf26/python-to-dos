787cfdb064bdbd7918f37a5d4c6935316986948d
gustavocsverdrup <gustavo.catalasverdrup@gmail.com>
2022-01-05 19:29:18 +0100

    adding readme

---
+++ README.md
@@ -0,0 +1,95 @@
+<div align="center" id="top"> 
+  <img src="./.github/app.gif" alt="To Dos" />
+
+&#xa0;
+
+  <!-- <a href="https://todos.netlify.app">Demo</a> -->
+</div>
+
+<h1 align="center">To Dos</h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/Gustaf26/to-dos?color=56BEB8">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/Gustaf26/to-dos?color=56BEB8">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/Gustaf26/to-dos?color=56BEB8">
+
+  <img alt="License" src="https://img.shields.io/github/license/Gustaf26/to-dos?color=56BEB8">
+
+</p>
+
+<!-- Status -->
+
+<!-- <h4 align="center">
+	🚧  To Dos 🚀 Under construction...  🚧
+</h4>
+
+<hr> -->
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0; 
+  <a href="#sparkles-features">Features</a> &#xa0; | &#xa0;
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="#memo-license">License</a> &#xa0; | &#xa0;
+  <a href="https://github.com/{{YOUR_GITHUB_USERNAME}}" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :dart: About
+
+This is a simple to-do app I found on the net using Python and SQLite, for you who want to get acquainted with how a simple Python-app can work with a database.
+
+The main functions are commented so that you can familiarize with SQL-queries and syntax and also with some basic Python code such as functions or loops
+
+You will need though to have installed SQLite and some basic knowledge of it in order to make it run
+
+Enjoy!
+
+## :sparkles: Features
+
+:heavy_check_mark: Feature 1;\
+:heavy_check_mark: Feature 2;\
+:heavy_check_mark: Feature 3;
+
+## :rocket: Technologies
+
+The following tools were used in this project:
+
+- [Python](https://docs.python.org/)
+- [SQLite](https://sqlite.org/)
+
+## :white_check_mark: Requirements
+
+Before starting :checkered_flag:, you need to have [Git](https://git-scm.com) installed.
+
+## :checkered_flag: Starting
+
+```bash
+# Clone this project
+$ git clone https://github.com/Gustaf26/to-dos
+
+# Access
+$ cd to-dos
+
+# Install dependencies
+$ yarn
+
+# Run the project
+$ yarn start
+
+# The server will initialize in the <http://localhost:3000>
+```
+
+## :memo: License
+
+This project is under license from MIT. For more details, see the [LICENSE](LICENSE.md) file.
+
+Made with :heart: by <a href="https://github.com/{{YOUR_GITHUB_USERNAME}}" target="_blank">{{YOUR_NAME}}</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
---
+++ main.py
@@ -0,0 +1,144 @@
+"""To-do list where you can add your tasks, modify them and delete if they have been completed.
+  A cleanup feature enables you to delete completed tasks"""
+
+"""I follow SQL syntax for queries, available at https://www.tutorialspoint.com/sql/sql-delete-query.htm"""
+
+from collections import OrderedDict
+import random
+import datetime
+import os
+import modify
+
+import sqlite3
+
+# sqlite3.connect is the connection object to the database
+con = sqlite3.connect('to_do_list.db')
+# This statement is related to the db cache and is important to have it present
+# It has to do with the updates in the db being synchronized with the cache in db file
+con.isolation_level = None
+# Con.cursor() is an inner object that gives us access to SQL Queries
+cur = con.cursor()
+
+class ToDo():
+    """Model for creating to-do items. 'done' indicates that it's been completed,
+    'protected' makes it immune to cleanup"""
+    #task = str
+    timestamp = datetime.datetime.today() + datetime.timedelta(hours=12)
+    #done = False
+    #protected = False
+
+    class Meta:
+         database = con
+
+
+def clear():
+    """Clear the display"""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def initialize():
+    """Connect to database"""
+    con
+
+# Function to view todos
+def view_entries(entries):
+
+    """"View to-do list"""
+    clear()
+    for row in entries:
+        print(row)
+
+    print('\nMY TO-DO LIST')
+    print('=' * 40)
+    return entries  
+
+# Function to add a new to do
+def add_entry(entries):
+    """Add a new task"""
+
+    new_task = input('\nTo do: ')
+    protect = 'No'
+    tasklimit = str(ToDo.timestamp)
+    taskinfo = (new_task, 'undone', protect, random.randint(0,10000000), tasklimit)
+    cur.execute("INSERT INTO mytodos VALUES(?,?,?,?,?)" , taskinfo)
+    entries = cur.execute('SELECT * FROM mytodos')
+    view_entries(entries)
+
+# Function to modify an existing to do taken by id
+def modify_entry(entries):
+    """Modify selected entry"""
+    id = input('Enter id of task')
+    print('\n\n')
+
+    # Loop through submenu to see what we wanna do with the to do
+    for key, value in sub_menu.items():
+        print('{}) {}'.format(key, sub_menu[key].__doc__))
+    print('q) Back to Main')
+    next_action = input('Action: ')
+
+    # Action to be taken, each action imported from modify.py
+    if next_action.lower().strip() in sub_menu:
+        sub_menu[next_action](cur, id)
+        entries = cur.execute('SELECT * FROM mytodos')
+        view_entries(entries)
+    else:
+        return
+
+
+def cleanup_entries(entries):
+    """Cleanup: delete of entries with status 'Done'"""
+    if (input('Are you sure you want to delete the done tasks? [yN]').lower().strip() == 'y'):
+        entries = cur.execute('SELECT * FROM mytodos')
+        for entry in entries:
+            if "Done" in entry:
+                print('hello')
+                cur.execute(f"DELETE FROM mytodos WHERE DONE = 'Done'")
+        entries = cur.execute('SELECT * FROM mytodos')
+        view_entries(entries)
+
+
+def menu_loop():
+    choice = None
+    entries = cur.execute('SELECT * FROM mytodos')
+    view_entries(entries)
+    while choice != 'q':
+        if entries:
+            print('\n' + '=' * 40 + '\n')
+
+        #The items() method returns a view object. 
+        # The view object contains the key-value pairs of the dictionary, 
+        # as tuples in a list.
+        for key, value in main_menu.items():
+            print('{}) {}'.format(key, value.__doc__))
+        print('q) Quit')
+
+        choice = input('\nAction: ')
+        if choice in main_menu:
+            try:
+                main_menu[choice](entries)
+            except ZeroDivisionError:
+                continue
+
+# An OrderedDict is a dictionary that remembers the order that keys were first inserted. 
+# The only difference between dict() and OrderedDict() is that:
+# OrderedDict preserves the order in which the keys are inserted. 
+# A regular dict doesn’t track the insertion order, and iterating it gives 
+# the values in an arbitrary order. By contrast, the order the items are 
+# inserted is remembered by OrderedDict.
+
+main_menu = OrderedDict([
+    ('a', add_entry),
+    ('m', modify_entry),
+    ('c', cleanup_entries)
+])
+
+sub_menu = OrderedDict([
+    ('m', modify.modify_task),
+    ('d', modify.toggle_done),
+    ('e', modify.delete_entry)
+])
+
+if __name__ == '__main__':
+    initialize()
+    menu_loop()
+    cur.close()
